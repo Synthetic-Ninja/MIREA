34e92694126001b101cdb3ff6316711f0833d13b
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-05-17 10:33:50 +0300

    Add GUI in client.py

--- SeverClient/client.py
+++ SeverClient/client.py
@@ -1,28 +1,78 @@
 import pickle
 import socket
-import argparse
+import tkinter
+from functools import partial
 
 
-
-def main(arguments: argparse.Namespace) -> None:
+def send_data(ip: tkinter.Label,
+              port: tkinter.Label,
+              x_min: tkinter.Label, 
+              x_max: tkinter.Label,
+              info_label: tkinter.Label) -> None:
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f'Try to send data to {arguments.target}\nx_min: {arguments.x_min};\nx_max: {arguments.x_max};')
     try:
-        s.connect((arguments.target, arguments.port))
-        s.sendall(pickle.dumps([arguments.x_min, arguments.x_max]))
+        s.connect((ip.get(), int(port.get())))
+        s.sendall(pickle.dumps([int(x_min.get()), int(x_max.get())]))
         data = s.recv(1024)
-        print(f'Message from server: {data.decode()}')
+        info_label['text'] = f'Message from server: {data.decode()}'
     except Exception as e:
-        print(f'Error: {e}')
+        info_label['text'] = f'Error: {e}'
 
+
+def main() -> None:
+    root = tkinter.Tk()
+    
+    default_ip = tkinter.StringVar()
+    default_ip.set("127.0.0.1")
+
+    default_port = tkinter.StringVar()
+    default_port.set("3030")
+
+    default_x_min = tkinter.StringVar()
+    default_x_min.set("-10")
+
+    default_x_max = tkinter.StringVar()
+    default_x_max.set("15")
+
+
+    label1 = tkinter.Label(root, text="Введите ip:")
+    label1.pack()
+    entry1 = tkinter.Entry(root, textvariable=default_ip)
+    entry1.pack(padx=10, pady=5)
+    
+    label2 = tkinter.Label(root, text="Введите порт:")
+    label2.pack()
+    entry2 = tkinter.Entry(root, textvariable=default_port)
+    entry2.pack(padx=10, pady=5)
+    
+    label3 = tkinter.Label(root, text="Введите x_min:")
+    label3.pack()
+    entry3 = tkinter.Entry(root, textvariable=default_x_min)
+    entry3.pack(padx=10, pady=5)
+    
+    label4 = tkinter.Label(root, text="Введите x_max:")
+    label4.pack()
+    entry4 = tkinter.Entry(root, textvariable=default_x_max)
+    entry4.pack(padx=10, pady=5)
+
+    info_label = tkinter.Label(root, text="")
+    info_label.pack()
+
+
+    button_command = partial(send_data,
+                             ip=entry1,
+                             port=entry2,
+                             x_min=entry3,
+                             x_max=entry4,
+                             info_label=info_label)
+    
+    button_submit = tkinter.Button(root, text="Отправить", command=button_command)
+    button_submit.pack(pady=10)
+    
+    root.mainloop()
 
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', type=str, default='127.0.0.1', help='Target address exapmle: -t 127.0.0.1')
-    parser.add_argument('-p', '--port', type=int, default=3030, help='Target port exapmle:  -p 3030')      # option that takes a value
-    parser.add_argument('-x1', '--x_min', type=int, default=-10)
-    parser.add_argument('-x2', '--x_max', type=int, default=11)
-    args = parser.parse_args()
-    main(args)
+    main()
 
